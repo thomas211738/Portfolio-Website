c36ade0c86780af209282afd83401ac85fe1c849
thomas211738 <109131481+thomas211738@users.noreply.github.com>
2023-06-23 23:28:14 -0400

    ic

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    VANTA.WAVES({
+        el: "#background",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x15041e,
+        shininess: 11.00,
+        waveHeight: 21.50,
+        waveSpeed: 1.25,
+        zoom: 0.98
+    });
+});
