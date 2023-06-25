0ff89daa1c498ba5ab54ff36f6652604b8cd9514
thomas211738 <thomas211738@gmail.com>
2023-06-25 14:11:22 -0400

    ch

--- script.js
+++ script.js
@@ -10,4 +10,20 @@ document.addEventListener("DOMContentLoaded", function() {
         scale: 1.00,
         scaleMobile: 1.00
       })
+      VANTA.WAVES({
+        el: "#background2",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color:0x23153c,
+        shininess: 90.00,
+        waveHeight: 17.00,
+        waveSpeed: 1.35,
+        zoom: 1.8
+        })
 });
+
